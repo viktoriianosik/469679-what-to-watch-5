3023f3f0b8584e92ae8c0c261aae624d4fba10b4
Viktoriia <50323249+viktoriianosik@users.noreply.github.com>
2020-09-30 10:33:48 +0300

    Личный проект: начинаем программировать (#3)
    * add mentor
    
    * installed dependencies webpack, eslint, babel
    
    * create components app, main

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/app/app';
 
+const FilmSetting = {
+  NAME: `The Grand Budapest Hotel`,
+  GENRE: `Drama`,
+  YEAR: 2014,
+};
+
+ReactDom.render(
+    <App name={FilmSetting.NAME} genre={FilmSetting.GENRE} year={FilmSetting.YEAR}/>,
+    document.querySelector(`#root`)
+);
