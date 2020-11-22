d38785c9a93625a54cea6c48da04dba0c6e1111a
Viktoriia <viktoria.nosik@gmail.com>
2020-11-22 16:04:12 +0200

    travis problems fixed

--- src/components/movie-details/movie-details.jsx
+++ src/components/movie-details/movie-details.jsx
@@ -1,5 +1,4 @@
 import React, {Fragment} from "react";
-import PropTypes from "prop-types";
 import {getTimeFromMins} from "../../utils";
 import MoviePropTypes from "../movie/movie-props";
 
--- src/components/movie-overview/movie-overview.jsx
+++ src/components/movie-overview/movie-overview.jsx
@@ -1,5 +1,4 @@
 import React, {Fragment} from "react";
-import PtopTypes from "prop-types";
 import MoviePropTypes from "../movie/movie-props";
 import {convertRatingToLevel} from "../../utils";
 
